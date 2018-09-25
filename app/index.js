469eb9a6801e0656a5732034b6ef8532876d3ca2
Student <example@example.com>
2018-09-25 14:47:47 -0700

    finalize media queries for basic layout

--- app/index.js
+++ app/index.js
@@ -4,6 +4,16 @@
 
 // Load application styles
 import 'styles/index.scss';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+UIkit.icon(element, options);
+
+
+UIkit.icon(element).svg.then(function(svg) { svg.querySelector('path').style.stroke = 'white'; })
 
 // ================================
 // START YOUR APP HERE
