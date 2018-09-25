c3d9b4c4abec279deb97b7e2612b406ac7fa36ee
Student <example@example.com>
2018-09-25 15:54:15 -0700

    add in mobile drop down menu

--- app/index.js
+++ app/index.js
@@ -3,18 +3,35 @@
  */
 
 // Load application styles
+import "materialize-loader";
+import "materialize-css";
 import 'styles/index.scss';
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import $ from 'jquery';
 
-// loads the Icon plugin
-UIkit.use(Icons);
+// // loads the Icon plugin
+// UIkit.use(Icons);
+//
+// UIkit.icon(element, options);
+//
+//
+// UIkit.icon(element).svg.then(function(svg) { svg.querySelector('path').style.stroke = 'white'; })
+//
+// // Show an off-canvas matching the passed CSS selector
+// UIkit.offcanvas.show('#my-id');
+//
+// // Hide any active offcanvas. Set force to true, if you don't want any animation.
+// UIkit.offcanvas.hide([force = false])
 
-UIkit.icon(element, options);
-
-
-UIkit.icon(element).svg.then(function(svg) { svg.querySelector('path').style.stroke = 'white'; })
 
 // ================================
 // START YOUR APP HERE
 // ================================
+
+$(document).ready(function() {
+  $("#mobileMenu").click(function() {
+    event.preventDefault();
+    $("#mobileDropDownContainer").toggle();
+  })
+});
